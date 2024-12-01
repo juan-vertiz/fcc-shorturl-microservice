b847ffd9c6b9d0ec609ff2366bfaef1e3f5a92a7
Juan Vertiz <juanpablovertiz@hotmail.com>
2024-12-01 16:35:50 -0600

    add: implement challenge solution

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const urls = [];
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +20,32 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res) => {
+  const nextShortUrl = urls.length + 1;
+  const originalUrl = new URL(req.body.url);
+  dns.lookup(originalUrl.hostname, (error, address, family) => {
+    if (error) {
+      res.send({error: 'invalid url'});
+    } else {
+      const urlEntry = {
+        original_url: originalUrl.href,
+        short_url: nextShortUrl
+      };
+      urls.push(urlEntry);
+      res.send(urlEntry);
+    }
+  });
+});
+
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const urlEntry = urls.find(element => element.short_url === Number(req.params.shortUrl));
+  if (urlEntry === undefined) {
+    res.send({error: 'not found'});
+  } else {
+    res.redirect(urlEntry.original_url);
+  }
 });
 
 app.listen(port, function() {
